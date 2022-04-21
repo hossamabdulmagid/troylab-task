94d51e1cb9670a4e068f0431aabbcf9eb6118df0
hossam <hosamabdulmaged@gmail.com>
2022-04-21 16:57:58 +0200

    updated cart design

--- src/App.styles.jsx
+++ src/App.styles.jsx
@@ -4,10 +4,9 @@ import styled from 'styled-components';
 export const RapperAppComponent = styled.div`
   .App-header {
     background-color: rgb(233, 235, 239);
-    min-height: 89vh;
+    height: 89vh;
     display: flex;
-    flex-direction: column;
-    align-items: center;
+      align-items: center;
     justify-content: center;
     font-size: calc(10px + 2vmin);
     color: black;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,32 @@
+import {Card} from 'react-bootstrap';
+
 const HomePage = () => {
+
     return (
         <div>
-            hello from Home
+            <div className={"container"}>
+                <div className={"row"}>
+                    {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(idx => {
+                        return (
+                            <>
+                                <div className={"col-sm-3"}>
+                                    <Card style={{width: '18rem', margin: '5px', padding: '5px', borderRadius: '5px'}}>
+                                        <Card.Body>
+                                            <Card.Title>Card Title</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                            <Card.Text>
+                                                Some quick example text to build on the card title
+
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </div>
+                            </>
+                        )
+                    })}
+                </div>
+            </div>
+
         </div>
     )
 }
